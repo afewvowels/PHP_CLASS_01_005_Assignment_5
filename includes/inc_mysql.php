f73e083e365699d062764b864f820057a76c018e
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2019-04-28 21:40:14 -0400

    update

--- includes/inc_mysql.php
+++ includes/inc_mysql.php
@@ -2,5 +2,5 @@
   DEFINE('HOST', 'localhost');
   DEFINE('USER', 'kbsmith01_user1');
   DEFINE('PASS', 'Mynewpassword2');
-  DEFINE('DB', 'kbsmith01_ecommerce');
+  DEFINE('DB', 'kbsmith01_cs');
 ?>
